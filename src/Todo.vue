fbfb32118fc2779b6ba1a71c7f8e249dfad73753
Evan Mattiza <emattiza@gmail.com>
2024-03-19 00:19:14 +0000

    wip

--- src/Todo.vue
+++ src/Todo.vue
@@ -1,56 +1,42 @@
-<script lang="ts">
+<script setup lang="ts">
 import { defineComponent, ref } from 'vue'
 import { useQuery } from '@tanstack/vue-query'
 import { useIntersectionObserver } from '@vueuse/core'
 
-export default defineComponent({
-  name: 'Todo',
-  props: {
-    id: { type: Number, required: true }
-  },
-  setup(props) {
-    const { id } = props
-    const target = ref(null)
-    const targetIsVisible = ref(false)
+const props = defineProps({
+  id: { type: Number, required: true }
+})
+const { id } = props
+const target = ref(null)
+const targetIsVisible = ref(false)
 
-    const { stop } = useIntersectionObserver(
-      target,
-      ([{ isIntersecting }], observerElement) => {
-        targetIsVisible.value = isIntersecting
-      },
+useIntersectionObserver(
+  target,
+  ([{ isIntersecting }], _observerElement) => {
+    targetIsVisible.value = isIntersecting
+  },
+)
+const url = `https://dummyjson.com/todos/${id}`
+const { data, error, isFetching, isPending } = useQuery({
+  queryKey: ['todo', id],
+  async queryFn() {
+    return await fetch(url).then(
+      (response) => response.json(),
     )
-    const url = `https://dummyjson.com/todos/${id}`
-    const { data, error, isFetching, isLoading } = useQuery({
-      queryKey: ['todo', id],
-      async queryFn() {
-        return await fetch(url).then(
-          (response) => response.json(),
-        )
-      },
-      enabled: targetIsVisible.value
-    })
-
-    return {
-      data,
-      target,
-      targetIsVisible,
-      error,
-      isFetching,
-      isLoading,
-    }
   },
+  enabled: targetIsVisible.value
 })
 </script>
 <template>
   <div ref="target" class="wrapper">
-    <template v-if="isLoading">Loading...</template>
+    <template v-if="isPending">Loading...</template>
     <template v-else-if="error">
-    'An error has occurred: {{ error.message }}
+      'An error has occurred: {{ error.message }}
     </template>
     <template v-else>
-       <div>ID: {{ data.id }}</div>
-       <div>Content: {{ data.todo}}</div>
-       <div>User: {{data.userId}}</div>
+      <div>ID: {{ data.id }}</div>
+      <div>Content: {{ data.todo }}</div>
+      <div>User: {{ data.userId }}</div>
     </template>
   </div>
 </template>
